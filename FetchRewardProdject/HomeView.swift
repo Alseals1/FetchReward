50c210bcba7c5a1c4de4c689ff2f8035b135098a
Alandis Seals <aseals@Alandiss-Ed-Farm-MacBook-Pro.local>
2022-03-09 12:16:19 -0600

    Setting up UI

---
+++ FetchRewardProdject/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FetchRewardProdject
+//
+//  Created by Alandis Seals on 3/8/22.
+//
+
+import UIKit
+
+class HomeView: UIViewController {
+    @IBOutlet weak var categoryCollectionView: UICollectionView!
+    @IBOutlet weak var mealCollectionView: UICollectionView!
+    @IBOutlet weak var popularCollectionView: UICollectionView!
+    
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FetchRewardProdject/Model/MealCategory.swift
@@ -0,0 +1,15 @@
+//
+//  DishCategory.swift
+//  FetchRewardProdject
+//
+//  Created by Alandis Seals on 3/9/22.
+//
+
+import Foundation
+import UIKit
+
+
+struct MealCategory {
+    let id, name: String
+    let image: UIImage
+}
